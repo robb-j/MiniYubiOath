848deb505a03f70468d19eabe150ee77fa4db86a
Rob Anderson <robanderson@hey.com>
2022-12-06 22:29:20 +0000

    working on USB parsing and structuring SwiftUI app

--- MiniYubiOath/AppMenu.swift
+++ MiniYubiOath/AppMenu.swift
@@ -17,263 +17,128 @@ struct AuthCode : Identifiable {
     let otp: Int
 }
 
-struct ProcessResult {
-    let exitCode: Int32
-    let output: String
-    let error: String
+
+enum YubiError: Error {
+    case notSupported
+    case notConnected
+    case cannotStart
 }
 
-/// https://www.hackingwithswift.com/example-code/system/how-to-run-an-external-program-using-process
-func exec(_ binary: String, args: [String]) throws -> ProcessResult {
-    let task = Process()
-    task.executableURL = URL(filePath: binary)
-    task.arguments = args
-    
-    let outputPipe = Pipe()
-    let errorPipe = Pipe()
-    
-    task.standardOutput = outputPipe
-    task.standardError = errorPipe
-    
-    try task.run()
-    task.waitUntilExit()
-    
-    let outputData = (try? outputPipe.fileHandleForReading.readToEnd()) ?? Data()
-    let errorData = (try? errorPipe.fileHandleForReading.readToEnd()) ?? Data()
-    
-    return ProcessResult(
-        exitCode: task.terminationStatus,
-        output: String(data: outputData, encoding: .utf8) ?? "",
-        error: String(data: errorData, encoding: .utf8) ?? ""
-    )
-}
-
-enum MiniYubiError: Error {
-    case ykmanNotInstalled
-    case badRead
-}
-
-func findYkman() throws -> String {
-    let result = try exec("/usr/bin/env", args: ["which", "ykman"])
-    
-    guard result.exitCode == 0 else {
-        throw MiniYubiError.ykmanNotInstalled
-    }
-    return result.output.trimmingCharacters(in: .whitespacesAndNewlines)
-}
-
-struct YKInfo {
-    let codes: [AuthCode]?
-    let error: String?
-    
-    static func codes(_ codes: [AuthCode]) -> YKInfo {
-        return YKInfo(codes: codes, error: nil)
-    }
-    static func error(_ error: String) -> YKInfo {
-        return YKInfo(codes: nil, error: error)
-    }
-}
-
-func getInfo() -> YKInfo {
-    getInfo2()
-    
-    do {
-        let ykman = try findYkman()
-        print("ykman=" + ykman)
-        
-        let result = try exec(ykman, args: ["oath", "accounts", "code"])
-        
-        
-        guard result.exitCode == 0 else {
-            print("Failed to read OATH codes")
-            print(result.output)
-            throw MiniYubiError.badRead
-        }
-        
-        
-        let regex = Regex {
-            Anchor.startOfLine
-            Capture {
-                OneOrMore(.anyNonNewline, .reluctant)
-            }
-            ":"
-            Capture {
-                OneOrMore(.anyNonNewline, .reluctant)
-            }
-            OneOrMore(.whitespace)
-            Capture {
-                Repeat(count: 6) {
-                    One(.digit)
-                }
-            }
-            Anchor.endOfLine
-        }
-        
-        let codes = result.output.matches(of: regex).map { match in
-            AuthCode(account: String(match.output.1), username: String(match.output.2), otp: Int(match.output.3)!)
-        }
-        return YKInfo.codes(codes)
-        
-    } catch MiniYubiError.ykmanNotInstalled {
-        return YKInfo.error("ykman not installed")
-    }
-    catch {
-        return YKInfo.error("YubiKey not connected")
-    }
-}
-
-//let YK_CHALLENGE_ALL_TAG =
-
-/// https://developers.yubico.com/OATH/YKOATH_Protocol.html#_calculate_all_instruction
-//struct APDU {
-//    let cla: Int32
-//    let ins: Int32
-//    let p1: Int32
-//    let p2: Int32
-//}
-//struct Instructions {
-//    static let calcAll = APDU(cla: 0x00, ins: 0xa4, p1: 0x00, p2: 0x00)
-//}
-//struct CalcAllChallenge {
-//    var tag: UInt8 = 0x74
-//    var value: UInt64
-//    init(date: Date) {
-//        value = UInt64(date.timeIntervalSinceNow / 30)
-//    }
-//    mutating func append(data: inout Data) {
-//        var binEnd = CFSwapInt64HostToBig(value)
-//        var valueSize = UInt8(MemoryLayout.size(ofValue: binEnd))
-//
-//        data.append(&tag, count: 1)
-//        data.append(&valueSize, count: 1)
-//        data.append(&binEnd, count: Int(valueSize))
-//    }
-//}
-
-//func appendByte(_ data: inout Data, byte: inout UInt8) {
-//    data.append(&byte, count: 1)
-//}
-
-func getInfo2() {
-    guard let manager = TKSmartCardSlotManager.default else { return }
-    
-//    guard let firstYubi = manager.slotNames.first(where: { $1.contains("YubiKey") }) else {
-//        return
-//    }
-    guard let firstYubi = manager.slotNames.first(where: { $0.contains("YubiKey") }) else { return }
-    
-    guard let slot = manager.slotNamed(firstYubi) else { return }
-    print(slot)
-    
-    guard let card = slot.makeSmartCard() else { return }
-    print(card)
-    
-    // var challenge = CalcAllChallenge(date: Date())
-    let date = Date()
-    let chal = UInt64(date.timeIntervalSince1970 / 30)
-    
-    var data = Data()
-    data.append(contentsOf: [
-        0x00, // cla
-        0xa4, // ins
-        0x00, // p1
-        0x00, // p2 (0x01 for truncated)
-        0x0a, // lc
-        0x74, // data[0] / tag
-        0x08, // data[1] / length of challenge
-    ])
-    
-    // Swift wouldn't let me put these bytes in the array above
-    data.append(UInt8((chal >>  0) & 0xFF)) // data[2] / challenge[0]
-    data.append(UInt8((chal >>  8) & 0xFF)) // data[3] / challenge[1]
-    data.append(UInt8((chal >> 16) & 0xFF)) // data[4] / challenge[2]
-    data.append(UInt8((chal >> 24) & 0xFF)) // data[5] / challenge[3]
-    data.append(UInt8((chal >> 32) & 0xFF)) // data[6] / challenge[4]
-    data.append(UInt8((chal >> 40) & 0xFF)) // data[7] / challenge[5]
-    data.append(UInt8((chal >> 48) & 0xFF)) // data[8] / challenge[6]
-    data.append(UInt8((chal >> 56) & 0xFF)) // data[9] / challenge[7]
-    
-//    print("Request", data)
-//
-//    card.beginSession { success, error in
-//        if let error = error {
-//            print("Failed to start session", error)
-//            return
-//        }
-//
-//        // TODO: run on a comms queue?
-//        // TODO: some kind of semaphore?
-//
-//        card.transmit(data) { response, error in
-//            if let error = error {
-//                print("Request failed", error)
-//                return
-//            }
-//            guard let response else {
-//                print("No response")
-//                return
-//            }
-//
-//            print("Success", response)
-//        }
-//    }
-}
-
-//func getCodes() -> [AuthCode] {
-//    let cmd = ["/opt/homebrew/bin/ykman", "oath", "accounts", "code"]
-//
-//    var data: [AuthCode] = []
-//    let process = Subprocess(cmd)
-//
-//    try? process.launch() { (process, outputData, errorData) in
-//        if process.exitCode == 0 {
-//            let str = String(data: outputData, encoding: .utf8)
-//            print(str)
-//        } else {
-//            print(String(data: errorData, encoding: .utf8))
-//        }
-//    }
-//
-//    process.waitForTermination()
-//
-//    return data
-//}
-
-// TODO: load the data in properly
-// TODO: handle multiple states / async
 // TODO: update every 30s on-the-30
-// TODO: get it working in the sandbox
-// TODO: bundle ykman in there somehow
-//       or communicate natively
-//       or try the c library
-// TODO: handle not-connected error
+// TODO: catch errors in Yubi#updateCodes
+// TODO: parse responses
+// TODO: run USB commands in a serial queue?
+// TODO: lock USB with a semaphore?
+
+@MainActor
+class Yubi: ObservableObject {
+    @Published private(set) var codes: [AuthCode] = []
+    @Published private(set) var message = "Loading…"
+    
+    func updateCodes() async {
+        codes = []
+        
+        do {
+            codes = try await readYubiKey()
+            message = "Connected"
+        } catch {
+            message = "Failed"
+        }
+    }
+    
+    func readYubiKey() async throws -> [AuthCode] {
+        guard let manager = TKSmartCardSlotManager.default else {
+            throw YubiError.notSupported
+        }
+        guard let firstYubi = manager.slotNames.first(where: { $0.contains("YubiKey") }) else {
+            throw YubiError.notConnected
+        }
+        
+        guard let slot = manager.slotNamed(firstYubi) else { throw YubiError.notConnected }
+        print(slot)
+        
+        guard let card = slot.makeSmartCard() else { throw YubiError.notConnected }
+        print(card)
+        
+        let date = Date()
+        let chal = UInt64(date.timeIntervalSince1970 / 30)
+        
+        let request = Data([
+            0x00, // cla
+            0xa4, // ins
+            0x00, // p1
+            0x00, // p2 (0x01 for truncated)
+            0x0a, // lc
+            0x74, // data[0] / tag
+            0x08, // data[1] / length of challenge
+            
+            UInt8((chal >>   0) & 0xFF), // data[2] / challenge[0]
+            UInt8((chal >>   8) & 0xFF), // data[3] / challenge[1]
+            UInt8((chal >>  16) & 0xFF), // data[4] / challenge[2]
+            UInt8((chal >>  24) & 0xFF), // data[5] / challenge[3]
+            UInt8((chal >>  32) & 0xFF), // data[6] / challenge[4]
+            UInt8((chal >>  40) & 0xFF), // data[7] / challenge[5]
+            UInt8((chal >>  48) & 0xFF), // data[8] / challenge[6]
+            UInt8((chal >>  56) & 0xFF), // data[9] / challenge[7]
+        ] as [UInt8])
+        
+        let connected = try await card.beginSession()
+        if !connected { throw YubiError.cannotStart }
+        
+        // Wait a little bit?
+        try await Task.sleep(for: .seconds(0.2))
+        
+        let response = try await card.transmit(request)
+        
+        print(response)
+        
+//        let b1 = response[0]
+//        let b2 = response[1]
+//        var key = UInt16()
+//        response.copyBytes(to: &key, from: Range(start: 0, end: 1))
+        
+        // 109      — 0
+        // 01101101 - 00000000
+        
+        // 0110110100000000
+        
+        // TODO: parse response
+        
+        return []
+    }
+}
+
+// YKFAPDUErrorCodeMoreData                 = 0x61
+
+
+// ...
 
 struct AppMenu: View {
-    var info = getInfo()
+    @StateObject private var yubi = Yubi()
     
     var body: some View {
-        if info.error != nil {
-            Text(info.error ?? "Something went wrong")
-            Button("Install ykman") {
-                guard let url = URL(string: "https://formulae.brew.sh/formula/ykman#default") else {
-                    fatalError("Invalid homebrew URL")
+        Button("Fetch") {
+            Task {
+                await yubi.updateCodes()
+            }
+        }
+        Text(yubi.message)
+        Group {
+            ForEach(yubi.codes) { code in
+                Button("\(code.account) — \(code.username)") {
+                    copy(code: code)
                 }
-                NSWorkspace.shared.open(url)
             }
         }
-        
-        ForEach(info.codes ?? []) { code in
-            Button("\(code.account) — \(code.username)") {
-                NSPasteboard.general.clearContents()
-                NSPasteboard.general.setString("\(code.otp)", forType: .string)
-            }
-        }
-
         Divider()
-        
         Button("Quit") {
             NSApplication.shared.terminate(nil)
         }.keyboardShortcut("q")
+    }
+    
+    func copy(code: AuthCode) {
+        NSPasteboard.general.clearContents()
+        NSPasteboard.general.setString("\(code.otp)", forType: .string)
     }
 }
 
--- MiniYubiOath/MiniYubiOathApp.swift
+++ MiniYubiOath/MiniYubiOathApp.swift
@@ -7,11 +7,24 @@
 
 import SwiftUI
 
+// TODO: the onChange doesn't get called, it seems SwiftUI doesn't trigger the scene stuff based on the menu itself
+// https://stackoverflow.com/questions/74354717
+
 @main
 struct MiniYubiOathApp: App {
+    @Environment(\.scenePhase) var scenePhase
+    
     var body: some Scene {
         MenuBarExtra("MiniYubiOATH", image: "StatusBarImage") {
             AppMenu()
+        }.onChange(of: scenePhase) { newPhase in
+            if newPhase == .active {
+                print("Became active")
+            } else if newPhase == .background {
+                print("Entered background")
+            } else if newPhase == .inactive {
+                print("Became inactive")
+            }
         }
     }
 }
